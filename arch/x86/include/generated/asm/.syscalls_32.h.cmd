6ca9503185a0fb66a8fa4f59043965428b60aabe
Abhinav Srivastava <abhisri1997@gmail.com>
2016-04-07 03:23:06 +0530

    Initial Stage

---
+++ arch/x86/include/generated/asm/.syscalls_32.h.cmd
@@ -0,0 +1,1 @@
+cmd_arch/x86/syscalls/../include/generated/asm/syscalls_32.h := /bin/bash '/home/abhi/lenovo/kernel/kernel-3.10/arch/x86/syscalls/syscalltbl.sh' /home/abhi/lenovo/kernel/kernel-3.10/arch/x86/syscalls/syscall_32.tbl arch/x86/syscalls/../include/generated/asm/syscalls_32.h
